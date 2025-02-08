9034fc5118877635e69a85c68ccf0271f36bd9de
kid-us <kidushh29@gmail.com>
2025-02-08 18:15:38 +0300

    Navbar comments removed.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,40 +1,10 @@
-import {
-  motion,
-  //   useScroll,
-  //   useMotionValueEvent,
-  //   AnimatePresence,
-} from "motion/react";
-// import { useState } from "react";
+import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  //   const { scrollY } = useScroll();
-  //   const [hidden, setHidden] = useState<boolean>(false);
-
-  //   useMotionValueEvent(scrollY, "change", (latest) => {
-  //     const previous = scrollY.getPrevious();
-  //     if (previous && latest > previous && latest > 80) {
-  //       setHidden(true);
-  //     } else {
-  //       setHidden(false);
-  //     }
-  //   });
-
   return (
     <>
-      {/* <AnimatePresence> */}
-      {/* {!hidden && ( */}
-      <motion.div
-        // exit={{ opacity: 0 }}
-        // initial={{ opacity: 0 }}
-        // animate={{ opacity: 1 }}
-        // transition={{
-        //   duration: 1,
-        // }}
-
-        className=" w-full top-0 left-0 z-50"
-      >
-        {/* <div className="container mx-auto "> */}
+      <motion.div className=" w-full top-0 left-0 z-50">
         <div className="flex justify-between pt-5 w-full mb-10">
           <Link
             to={"/"}
@@ -43,10 +13,7 @@ const Navbar = () => {
             {"{dev}"}
           </Link>
         </div>
-        {/* </div> */}
       </motion.div>
-      {/* )} */}
-      {/* </AnimatePresence> */}
     </>
   );
 };
