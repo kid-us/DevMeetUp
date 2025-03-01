76cd0ac0b68f792c16c71685b09b95f10f76c9ce
kid-us <kidushh29@gmail.com>
2025-03-01 19:20:25 +0300

    V2 Hero section added

--- src/components/Pages/V2.tsx
+++ src/components/Pages/V2.tsx
@@ -1,7 +1,42 @@
+import { BookHeart, CalendarFold, MapPinHouse, User } from "lucide-react";
+import EventDetail, { Event } from "../EventDetail";
+
 const Version2 = () => {
+  const Event: Event[] = [
+    {
+      id: 1,
+      icon: CalendarFold,
+      title: "Date",
+      content: "Mar 15, 2024 | 9:00 AM",
+    },
+    {
+      id: 2,
+      icon: MapPinHouse,
+      title: "Venue",
+      content: "ALX Hub, Addis Ababa",
+    },
+    {
+      id: 3,
+      icon: BookHeart,
+      title: "Organizers",
+      content: "CodeNight Community Admins",
+    },
+    {
+      id: 4,
+      icon: User,
+      title: "Participants",
+      content: "200+ developers and tech professionals",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Version 2 Content Here.</h1>
+    <div className="pt-16">
+      <EventDetail
+        title="Dev MeetUp v2"
+        subTitle="We are Back with Bigger Blast :)"
+        description="Join us for an exciting day of tech talks, workshops, and networking opportunities!"
+        icons={Event}
+      />
     </div>
   );
 };
